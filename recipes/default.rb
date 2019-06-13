07c2c782895298264d1cb8e54193ed7af01faae6
Tech-A34 <tech-a34@Tech-A34s-MacBook-Air.local>
2019-06-13 09:52:24 +0100

    proxy-port removed

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,21 +15,21 @@ service 'nginx' do
   action [:enable, :start]
 end
 
-template '/etc/nginx/sites-available/proxy.conf' do
-  source 'proxy.conf.erb'
-  variables proxy_port: node['nginx']['proxy_port']
-  notifies :restart, 'service[nginx]'
-end
-
-link '/etc/nginx/sites-enabled/proxy.conf' do
-  to '/etc/nginx/sites-available/proxy.conf'
-  notifies :restart, 'service[nginx]'
-end
-
-link '/etc/nginx/sites-enabled/default' do
-  notifies :restart, 'service[nginx]'
-  action :delete
-end
+# template '/etc/nginx/sites-available/proxy.conf' do
+#   source 'proxy.conf.erb'
+#   variables proxy_port: node['nginx']['proxy_port']
+#   notifies :restart, 'service[nginx]'
+# end
+#
+# link '/etc/nginx/sites-enabled/proxy.conf' do
+#   to '/etc/nginx/sites-available/proxy.conf'
+#   notifies :restart, 'service[nginx]'
+# end
+#
+# link '/etc/nginx/sites-enabled/default' do
+#   notifies :restart, 'service[nginx]'
+#   action :delete
+# end
 
 include_recipe "nodejs"
 
